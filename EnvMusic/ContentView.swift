3e099c0db4a66e20d80add5e3cae7986ee96006b
susuyan <susuyan@163.com>
2024-08-20 13:51:46 +0800

    Initial Commit

---
+++ EnvMusic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  EnvMusic
+//
+//  Created by 苏苏炎 on 2024/8/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ EnvMusic/EnvMusicApp.swift
@@ -0,0 +1,17 @@
+//
+//  EnvMusicApp.swift
+//  EnvMusic
+//
+//  Created by 苏苏炎 on 2024/8/20.
+//
+
+import SwiftUI
+
+@main
+struct EnvMusicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
